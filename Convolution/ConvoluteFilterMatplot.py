76cf34d0d95a4126e1e91f2a781ecc6ee0f77513
025818786k <akaiki>
2020-07-10 23:58:05 +0700

    Update tien do bao cao, con chuong 4

--- Convolution/ConvoluteFilterMatplot.py
+++ Convolution/ConvoluteFilterMatplot.py
@@ -7,12 +7,26 @@ import numpy as np
 import os
 
 ptr.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
-# import convolute_lib as conv
+img_path = 'testSoXe.png'
 
-img_path = 'test_hand_write.png'
+# img = cv2.imread(img_path, cv2.COLOR_BGR2GRAY)
+# hImg, wImg, _ = img.shape
+# conf = r'--oem 3 --psm 6 outputbase digits'
+# boxes = ptr.image_to_data(img)
+# for a,b in enumerate(boxes.splitlines()):
+#         # print(b)
+#         if a!=0:
+#             b = b.split()
+#             if len(b)==12:
+#                 x,y,w,h = int(b[6]),int(b[7]),int(b[8]),int(b[9])
+#                 cv2.putText(img,b[11],(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,(50,50,255),2)
+#                 cv2.rectangle(img, (x,y), (x+w, y+h), (50, 50, 255), 2)
+
+# cv2.imshow('boxed', img)
+# cv2.waitKey(0)
 
 #xám hóa ảnh đàu vào
-img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+img = cv2.imread(img_path, cv2.COLOR_BGR2GRAY)
 
 identity = np.array((
     [0, 0, 0],
@@ -20,9 +34,9 @@ identity = np.array((
     [0, 0, 0]))
 
 edge = np.array((
-    [0,  1,  0],
-    [1, -4,  1],
-    [0,  1,  0]))
+    [-1,  -1,  -1],
+    [-1, 8,  -1],
+    [-1,  -1,  -1]))
 
 boxblur = (1.0 / 9) * np.array(
     [[1, 1, 1],
@@ -49,9 +63,9 @@ largeBlur = np.ones((21, 21), dtype="float") * (1.0 / (21 * 21))
 
 # construct a sharpening filter
 sharpen = np.array((
-    [0, -2, 0],
-    [-2, 10, -2],
-    [0, -2, 0]))
+    [0, -1, 0],
+    [-1, 5, -1],
+    [0, -1, 0]))
 
 laplacian = (1.0 / 16) * np.array(
     [[ 0,  0, -1,  0,  0],
@@ -80,21 +94,22 @@ sobelBottom = np.array((
     [ 0,  0,  0],
     [-1, -2, -1]))
 
+
 filters = [
-    ("Identity", identity),
-    ("Edge", edge),
-    ("Box Blur", boxblur),
-    ("Square", square),
-    ("Gaussian", gaussian),
-    ("Emboss", emboss),
-    ("Small blur", smallBlur),
-    ("Large blur", largeBlur),
-    ("Sharpen", sharpen),
-    ("Laplacian", laplacian),
-    ('Sobel Left', sobelLeft),
-    ('Sobel Right', sobelRight),
-    ('Sobel Top', sobelTop),
-    ('Sobel Bottom', sobelBottom)
+    ("Identity", identity), #0
+    ("Edge", edge), #1
+    ("Box Blur", boxblur), #2
+    ("Square", square), #3
+    ("Gaussian", gaussian), #4
+    ("Emboss", emboss), #5
+    ("Small blur", smallBlur), #6
+    ("Large blur", largeBlur), #7
+    ("Sharpen", sharpen), #8
+    ("Laplacian", laplacian), #9 
+    ('Sobel Left', sobelLeft), #10
+    ('Sobel Right', sobelRight), #11
+    ('Sobel Top', sobelTop), #12
+    ('Sobel Bottom', sobelBottom) #13
 ]
 
 fig = plt.figure(figsize=(12, 8))
@@ -115,16 +130,22 @@ plt.show()
 
 # def show(i, filter, name):
 #     img_out = cv2.filter2D(img, -1, filter[1])
-#     print("------Filter "+str(i)+": "+filter[0]+"------\n")
+#     print("------Filter "+str(i+1)+": "+filter[0]+"------\n")
 #     print(ptr.image_to_string(img_out))
 #     print("\n-------------------------------------")
     
 #     cv2.imshow(name, img_out)
 #     cv2.waitKey(0)
 
-
 # for i, filter in enumerate(filters):
 #     name = filter[0]
 #     show(i,filter, name)
-    
-    
+
+# img_out = cv2.filter2D(img, 0,emboss[1])
+# img_out = cv2.filter2D(img_out, 0,gaussian[1])   
+# img_out = cv2.filter2D(img_out, 0,sharpen[1])
+# img_out = cv2.filter2D(img_out, 0,boxblur[1])
+# img_out = cv2.filter2D(img_out, 0,edge[1])
+# print(ptr.image_to_string(img_out))
+# cv2.imshow("name", img_out)
+# cv2.waitKey(0)
